4fcfdd8bf6c64f9d84ed92a87802a008e0522ddf
038bkn <038bkn@gmail.com>
2024-08-16 14:55:54 +0900

    ビルドできた

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // base: process.env.GITHUB_PAGES ? "/react-sample/" : "./",
+  base: "/react-sample/",
+  build: {
+    outDir: "docs",
+  },
+});
