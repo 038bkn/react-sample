6b2d175613ea04f275c7ff619080f5f0014d31fd
038bkn <038bkn@gmail.com>
2024-08-16 15:25:46 +0900

    feat: Update text in App component and script source in index.html
    カウントしちゃうよ～"というメッセージにAppコンポーネントのテキストを変更しました。さらに、index.htmlのスクリプトソースを"/react-sample/assets/index-DRQTfWsh.js"に更新しました。

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,10 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [error, setError] = useState('')
+
   console.log(count)
+
   return (
     <>
       <h1>カウントしちゃうよ～</h1>
@@ -11,22 +14,27 @@ function App() {
       <CountPreview hoge={count} />
       <button onClick={function () {
         setCount(count + 1)
+        setError('') // エラーメッセージをクリア
         console.log(count)
       }}>+</button>
 
       <button onClick={function () {
         if (count > 0) {
           setCount(count - 1)
+          setError('') // エラーメッセージをクリア
           console.log(count)
-          console.log('押せないよ～ん')
+        } else {
+          setError('押せないよ～ん')
         }
       }}>-</button>
+
+      {error && <div style={{ color: 'red' }}>{error}</div>}
     </>
   )
 }
 
 const CountPreview = function ({ hoge }: { hoge: number }) {
-  return <div>count:{hoge}</div>
+  return <div>count: {hoge}</div>
 }
 
 export default App
